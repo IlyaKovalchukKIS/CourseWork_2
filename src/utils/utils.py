684ce0d5bf32d0fb9dd7e25a19b9944351bc2797
ilya <mrlivedance@vk.com>
2023-05-10 22:02:33 +0300

    add dir src/main src/utils, and add functions load_words_subwords and random_word_in_list

---
+++ src/utils/utils.py
@@ -0,0 +1,18 @@
+from random import choice
+
+import requests
+
+WORDS_SUBWORDS = "https://www.jsonkeeper.com/b/2Y4S"
+
+
+# WORDS_SUBWORDS = "https://www.jsonkeeper.com/b/WO2Y"
+
+def load_words_subwords(list_word: str):
+    f = requests.get(list_word)
+    result = f.json()
+    return result
+
+
+def random_word_in_list(list_word: str):
+    return choice(list_word)
+
